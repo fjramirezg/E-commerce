ecf552c169cf0cd2db36d762b219368ec9b6e01c
Frank Ramirez <89478677+fjramirezg@users.noreply.github.com>
2022-06-17 08:37:18 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,37 @@
-
 "use strict"
 
 const items = [
     {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'https://academlo-store.netlify.app/assets/img/featured1.png',
-      category: 'hoodies',
-      quantity: 10
+        id: 1,
+        name: 'Hoodies',
+        price: 14.00,
+        image: 'https://academlo-store.netlify.app/assets/img/featured1.png',
+        category: 'hoodies',
+        quantity: 10
     },
     {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'https://academlo-store.netlify.app/assets/img/featured2.png',
-      category: 'shirts',
-      quantity: 15
+        id: 2,
+        name: 'Shirts',
+        price: 24.00,
+        image: 'https://academlo-store.netlify.app/assets/img/featured2.png',
+        category: 'shirts',
+        quantity: 15
     },
     {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
-      category: 'sweatshirts',
-      quantity: 20
+        id: 3,
+        name: 'Sweatshirts',
+        price: 24.00,
+        image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+        category: 'sweatshirts',
+        quantity: 20
     },
     {
-      id: 4,
-      name: 'Sweatshirts',
-      price: 30.00,
-      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
-      category: 'sweatshirts',
-      quantity: 10
+        id: 4,
+        name: 'Sweatshirts',
+        price: 30.00,
+        image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+        category: 'sweatshirts',
+        quantity: 10
     }
 ]
 
@@ -46,15 +45,24 @@ let cart = []
 
 
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
+
+    if (window.localStorage.getItem("cart")) {
+        cart = JSON.parse(window.localStorage.getItem("cart"))
+    } else {
+        window.localStorage.setItem("cart", JSON.stringify(cart))
+    }
+
     mostrarProductos()
+    mostrarProductosCart()
+
 })
 
-cartIcon.addEventListener( "click", () =>{
+cartIcon.addEventListener("click", () => {
     cartOverlay.classList.add("mostrar")
 })
 
-cartClose.addEventListener( "click", () =>{
+cartClose.addEventListener("click", () => {
     cartOverlay.classList.remove("mostrar")
 })
 
@@ -64,27 +72,27 @@ cartClose.addEventListener( "click", () =>{
 
 let header = document.querySelector("header")
 
-window.addEventListener( "scroll", () =>{
-    if( window.scrollY > 60 ){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 60) {
         header.classList.add("scroll-header")
-    }else{
+    } else {
         header.classList.remove("scroll-header")
     }
 })
 
 
 function mostrarProductos() {
-    
+
     let productsButton = document.querySelectorAll(".btn-primary")
 
 
-    productsButton.forEach( (button) =>{
-        button.addEventListener("click", (evento) =>{
-            let id = parseInt( button.getAttribute("data-id") )
-            let product = items.find( item =>{ 
-                return item.id === id 
+    productsButton.forEach((button) => {
+        button.addEventListener("click", (evento) => {
+            let id = parseInt(button.getAttribute("data-id"))
+            let product = items.find(item => {
+                return item.id === id
             })
-            
+
             agregarProducto(product)
             // cart.push( product )
             //console.log((cart))
@@ -93,22 +101,22 @@ function mostrarProductos() {
 }
 
 
-function agregarProducto( producto ){
+function agregarProducto(producto) {
 
-    let resultadoFind = cart.find( item => item.id === producto.id )
-    
+    let resultadoFind = cart.find(item => item.id === producto.id)
 
-    if( resultadoFind ){
+
+    if (resultadoFind) {
         let stock = cart[resultadoFind.index].quantity
-        let quantitySelected = cart[resultadoFind.index].quantitySelected 
+        let quantitySelected = cart[resultadoFind.index].quantitySelected
 
-        if( stock > quantitySelected ){
+        if (stock > quantitySelected) {
             cart[resultadoFind.index].quantitySelected += 1
-        }else{
-            alert( "No tenemos suficiente inventario" )
+        } else {
+            alert("No tenemos suficiente inventario")
         }
 
-    }else{
+    } else {
         producto.quantitySelected = 1
         producto.index = cart.length
 
@@ -118,16 +126,18 @@ function agregarProducto( producto ){
 
     console.log(cart)
     mostrarProductosCart()
+
+    window.localStorage.setItem("cart", JSON.stringify(cart))
 }
 
 
-function mostrarProductosCart(){
+function mostrarProductosCart() {
 
     let fragmentoHTML = ``
     let suma = 0
     let cantidadTotal = 0
 
-    cart.forEach( item =>{
+    cart.forEach(item => {
         fragmentoHTML += `
         <div class="cart-item">
             <img src=${item.image} alt="">
@@ -136,7 +146,7 @@ function mostrarProductosCart(){
         </div>
         `
 
-        let totalProducto = item.quantitySelected * item.price 
+        let totalProducto = item.quantitySelected * item.price
         suma += totalProducto
 
         cantidadTotal += item.quantitySelected
@@ -144,10 +154,13 @@ function mostrarProductosCart(){
 
     fragmentoHTML += `
     <div class="cart-price">
-        <p>Productos seleccionados:${ cantidadTotal }</p>
-        <p>$${ suma }</p>
+        <p>Productos seleccionados:${cantidadTotal}</p>
+        <p>$${suma}</p>
     </div>
     `
     cartContainer.innerHTML = fragmentoHTML
     cartCount.textContent = cantidadTotal
+
+
+
 }
